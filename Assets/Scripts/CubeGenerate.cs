caf0b7cfdc555c333af51b579d4a8815ea2ffaa1
vanlicht <vanlicht@gmail.com>
2016-12-15 09:00:37 -0800

    generate cube with coroutine, without mesh

---
+++ Assets/Scripts/CubeGenerate.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class CubeGenerate : MonoBehaviour
+{
+    #region Public Variables
+    public int xSize, ySize, zSize;
+    #endregion
+
+    #region Private Variables
+    private Vector3[] vertices;
+    private Mesh mesh;
+    private int cornerVertices;
+    private int edgeVertices;
+    private int faceVertices;
+    #endregion
+
+    #region Main Methods
+    private void Awake()
+    {
+        StartCoroutine(Generate());
+    }
+    #endregion
+
+    #region
+    IEnumerator Generate()
+    {
+        this.GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "ProceduralCube";
+        WaitForSeconds wait = new WaitForSeconds(0.05f);
+
+        cornerVertices = 8;
+        edgeVertices = 4 * (xSize + ySize + zSize - 3);
+        faceVertices = 2 * ((xSize - 1) * (ySize - 1) + (ySize - 1) * (zSize - 1) + (xSize - 1) * (zSize - 1));
+        vertices = new Vector3[cornerVertices + edgeVertices + faceVertices];
+
+        int v = 0;
+        for(int y = 0; y <=ySize; y++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                vertices[v++] = new Vector3(x, y, 0);
+                Debug.Log(v);
+                yield return wait;
+            }
+
+            for (int z = 1; z <= zSize; z++)
+            {
+                vertices[v++] = new Vector3(xSize, y, z);
+                Debug.Log(v);
+                yield return wait;
+            }
+
+            for (int x = xSize - 1; x >= 0; x--)
+            {
+                vertices[v++] = new Vector3(x, y, zSize);
+                Debug.Log(v);
+                yield return wait;
+            }
+
+            for (int z = zSize - 1; z > 0; z--)
+            {
+                vertices[v++] = new Vector3(0, y, z);
+                Debug.Log(v);
+                yield return wait;
+            }
+        }
+
+        for(int z = 1; z < zSize; z++)
+        {
+            for(int x = 1; x < xSize; x++)
+            {
+                vertices[v++] = new Vector3(x, ySize, z);
+                Debug.Log(v);
+                yield return wait;
+            }
+        }
+
+        for (int z = 1; z < zSize; z++)
+        {
+            for (int x = 1; x < xSize; x++)
+            {
+                vertices[v++] = new Vector3(x, 0, z);
+                Debug.Log(v);
+                yield return wait;
+            }
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(vertices == null)
+        {
+            return;
+        }
+        Gizmos.color = Color.black;
+        for(int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], 0.05f);
+        }
+    }
+    #endregion
+}
--- Assets/Scripts/Grid_01_simple.cs
+++ Assets/Scripts/Grid_01_simple.cs
@@ -13,34 +13,56 @@ public class Grid_01_simple : MonoBehaviour
     #region Private Variables
     private Vector3[] vertices;
     private Mesh mesh;
+    private Vector2[] uv;
+    Vector4[] tangents;
+    Vector4 tangent;
     #endregion
 
     #region Main Methods
     private void Awake()
     {
-        StartCoroutine(Generate());
+        Generate();
     }
     #endregion
 
     #region Utility Methods
-    private IEnumerator Generate()
+    void Generate()
     {
-        WaitForSeconds wait = new WaitForSeconds(0.05f);
-
         GetComponent<MeshFilter>().mesh = mesh = new Mesh();
         mesh.name = "ProceduralGrid";
 
         vertices = new Vector3[(xSize + 1) * (ySize + 1)];
-        for(int i = 0, y = 0; y < ySize + 1; y++)
+        
+        //for uv
+        uv = new Vector2[vertices.Length];
+        
+        //for normal
+        tangents = new Vector4[vertices.Length];
+
+        /*The surface normal represents upward in this space, but which way is right? That's defined by the tangent. 
+        Ideally, the angle between these two vectors is 90°. The cross product of them yields the third direction needed to define 3D space. 
+        In reality the angle is often not 90° but the results are still good enough.
+        So a tangent is a 3D vector, but Unity actually uses a 4D vector. Its fourth component is always either −1 or 1, 
+        which is used to control the direction of the third tangent space dimension – either forward or backward. 
+        This facilitates mirroring of normal maps, which is often used in 3D models of things with bilateral symmetry, like people. 
+        The way Unity's shaders perform this calculation requires us to use −1.
+        */
+        tangent = new Vector4(1, 0, 0, -1);
+        
+
+        for (int i = 0, y = 0; y < ySize + 1; y++)
         {
             for(int x = 0; x < xSize + 1; x++, i++)
             {
                 vertices[i] = new Vector3(x, y);
-                yield return wait;
+                uv[i] = new Vector2((float)x / xSize, (float)y / ySize);
+                tangents[i] = tangent;
             }
         }
 
         mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.tangents = tangents;
 
         ////Pattern for the first square, composed of 2 triangles
         //int[] triangles = new int[6];
@@ -58,10 +80,12 @@ public class Grid_01_simple : MonoBehaviour
                 triangles[ti + 3] = triangles[ti + 2] = vi + 1;
                 triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
                 triangles[ti + 5] = vi + xSize + 2;
-                yield return wait;
-                mesh.triangles = triangles;
+                
             }
         }
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+
     }
 
     private void OnDrawGizmos()
